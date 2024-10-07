079594fd9ea60c01bd027281e0155c31cb4d9746
Taylor Lo <taylorloph@gmail.com>
2024-10-07 09:21:30 -0400

    Income or Expense Transaction

--- components/transaction.vue
+++ components/transaction.vue
@@ -4,7 +4,7 @@
   >
     <div class="flex items-center justify-between">
       <div class="flex items-center space-x-1">
-        <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
+        <UIcon :name="icon" :class="iconColor" />
         <div>{{ transaction.description }}</div>
       </div>
       <div>
@@ -30,6 +30,17 @@
 const props = defineProps({
   transaction: Object,
 });
+
+const isIncome = computed(() => props.transaction.type === "Income");
+
+const icon = computed(() =>
+  isIncome.value ? "i-heroicons-arrow-up-right" : "i-heroicons-arrow-down-left"
+);
+
+const iconColor = computed(() =>
+  isIncome.value ? "text-green-600" : "text-red-600"
+);
+
 const { currency } = useCurrency(props.transaction.amount);
 const items = [
   [
